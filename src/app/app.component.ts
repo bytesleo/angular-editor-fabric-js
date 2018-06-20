68ee91041480b76f49c98411c566040aac61b1e9
Jonathan Alves <alves_aj@windowslive.com>
2018-06-20 20:16:46 -0300

    added better controls, change image render, update to textbox, added objects to canvas center

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   private canvas: any;
   private props: any = {
     canvasFill: '#ffffff',
+    centered: {},
     canvasImage: '',
     id: null,
     opacity: null,
@@ -50,7 +51,8 @@ export class AppComponent {
     this.canvas = new fabric.Canvas('canvas', {
       hoverCursor: 'pointer',
       selection: true,
-      selectionBorderColor: 'blue'
+      selectionBorderColor: 'blue',
+      preserveObjectStacking: true
     });
 
     this.canvas.on({
@@ -65,6 +67,7 @@ export class AppComponent {
         // selectedObject.cornerColor = 'rgba(255, 87, 34, 0.7)';
 
         this.resetPanels();
+        this.customControls(selectedObject);
 
         if (selectedObject.type !== 'group' && selectedObject) {
 
@@ -78,7 +81,7 @@ export class AppComponent {
               this.figureEditor = true;
               this.getFill();
               break;
-            case 'i-text':
+            case 'textbox':
               this.textEditor = true;
               this.getLineHeight();
               this.getCharSpacing();
@@ -101,8 +104,7 @@ export class AppComponent {
       }
     });
 
-    this.canvas.setWidth(this.size.width);
-    this.canvas.setHeight(this.size.height);
+    this.changeSize();
 
     // get references to the html canvas element & its context
     // this.canvas.on('mouse:down', (e) => {
@@ -117,25 +119,26 @@ export class AppComponent {
 
   //Block "Size"
 
-  changeSize(event: any) {
+  changeSize() {
     this.canvas.setWidth(this.size.width);
     this.canvas.setHeight(this.size.height);
+    this.updateCentered();
   }
 
   //Block "Add text"
 
   addText() {
     let textString = this.textString;
-    let text = new fabric.IText(textString, {
-      left: 10,
-      top: 10,
+    let text = new fabric.Textbox(textString, {
+      ...this.props.centered,
       fontFamily: 'helvetica',
       angle: 0,
       fill: '#000000',
       scaleX: 0.5,
       scaleY: 0.5,
       fontWeight: '',
-      hasRotatingPoint: true
+      fontSize: 54,
+      // hasRotatingPoint: true
     });
     this.extend(text, this.randomId());
     this.canvas.add(text);
@@ -146,23 +149,7 @@ export class AppComponent {
   //Block "Add images"
 
   getImgPolaroid(event: any) {
-    let el = event.target;
-    fabric.Image.fromURL(el.src, (image) => {
-      image.set({
-        left: 10,
-        top: 10,
-        angle: 0,
-        padding: 10,
-        cornersize: 10,
-        hasRotatingPoint: true,
-        peloas: 12
-      });
-      image.setWidth(150);
-      image.setHeight(150);
-      this.extend(image, this.randomId());
-      this.canvas.add(image);
-      this.selectItemAfterAdded(image);
-    });
+    this.addImageOnCanvas(event.target.src);
   }
 
   //Block "Upload Image"
@@ -171,8 +158,7 @@ export class AppComponent {
     if (url) {
       fabric.Image.fromURL(url, (image) => {
         image.set({
-          left: 10,
-          top: 10,
+          ...this.props.centered,
           angle: 0,
           padding: 10,
           cornersize: 10,
@@ -192,6 +178,7 @@ export class AppComponent {
       var reader = new FileReader();
       reader.onload = (event) => {
         this.url = event.target['result'];
+        this.addImageOnCanvas(this.url);
       }
       reader.readAsDataURL(event.target.files[0]);
     }
@@ -208,24 +195,28 @@ export class AppComponent {
     switch (figure) {
       case 'rectangle':
         add = new fabric.Rect({
-          width: 200, height: 100, left: 10, top: 10, angle: 0,
+          ...this.props.centered,
+          width: 200, height: 100, angle: 0,
           fill: '#3f51b5'
         });
         break;
       case 'square':
         add = new fabric.Rect({
-          width: 100, height: 100, left: 10, top: 10, angle: 0,
+          ...this.props.centered,
+          width: 100, height: 100, angle: 0,
           fill: '#4caf50'
         });
         break;
       case 'triangle':
         add = new fabric.Triangle({
-          width: 100, height: 100, left: 10, top: 10, fill: '#2196f3'
+          ...this.props.centered,
+          width: 100, height: 100, fill: '#2196f3'
         });
         break;
       case 'circle':
         add = new fabric.Circle({
-          radius: 50, left: 10, top: 10, fill: '#ff5722'
+          ...this.props.centered,
+          radius: 50, fill: '#ff5722'
         });
         break;
     }
@@ -277,6 +268,29 @@ export class AppComponent {
   }
 
   /*------------------------Global actions for element------------------------*/
+
+  updateCentered() {
+    this.props.centered = {
+      left: this.canvas.getWidth() * 0.5,
+      top: this.canvas.getHeight() * 0.5,
+      originX: 'center',
+      originY: 'center',
+    };
+  }
+
+  customControls = (object) => {
+      object.set({
+          centeredRotation: true,
+          cornerStyle: 'circle',
+          borderColor: '#888',
+          cornerColor: '#fff',
+          cornerStrokeColor: '#888',
+          transparentCorners: false,
+          cornerSize: 18
+      });
+      object.setControlVisible('mt', false);
+      object.setControlVisible('mb', false);
+  }
 
   getActiveStyle(styleName, object) {
     object = object || this.canvas.getActiveObject();
@@ -337,8 +351,8 @@ export class AppComponent {
         case 'triangle':
           clone = new fabric.Triangle(activeObject.toObject());
           break;
-        case 'i-text':
-          clone = new fabric.IText('', activeObject.toObject());
+        case 'textbox':
+          clone = new fabric.Textbox('', activeObject.toObject());
           break;
         case 'image':
           clone = fabric.util.object.clone(activeObject);
