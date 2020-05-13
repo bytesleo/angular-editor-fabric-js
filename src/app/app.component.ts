7d4fc5e42ff128c9fb27f8dab1c869ed4d06f848
Marie-Charlotte Coutaud <marie-charlotte.coutaud@axceta.com>
2020-05-13 14:50:13 -0400

    Move css to the app and add link between the app and the library for each function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,131 @@ export class AppComponent {
 
   @ViewChild('canvas', {static: false}) canvas: FabricjsLibraryComponent;
 
+  public rasterize() {
+    this.canvas.rasterize();
+  }
+
+  public rasterizeSVG() {
+    this.canvas.rasterizeSVG();
+  }
+
+  public saveCanvasToJSON() {
+    this.canvas.saveCanvasToJSON();
+  }
+
+  public loadCanvasFromJSON() {
+    this.canvas.loadCanvasFromJSON();
+  }
+
+  public confirmClear() {
+    this.canvas.confirmClear();
+  }
+
+  public changeSize() {
+    this.canvas.changeSize();
+  }
+
+  public addText() {
+    this.canvas.addText();
+  }
+
+  public getImgPolaroid(event) {
+    this.canvas.getImgPolaroid(event);
+  }
+
+  public addImageOnCanvas(url) {
+    this.canvas.addImageOnCanvas(url);
+  }
+
+  public readUrl(event) {
+    this.canvas.readUrl(event);
+  }
+
+  public removeWhite(url) {
+    this.canvas.removeWhite(url);
+  }
+
+  public addFigure(figure) {
+    this.canvas.addFigure(figure);
+  }
+
   public removeSelected() {
     this.canvas.removeSelected();
+  }
+
+  public sendToBack() {
+    this.canvas.sendToBack();
+  }
+
+  public bringToFront() {
+    this.canvas.bringToFront();
+  }
+
+  public clone() {
+    this.canvas.clone();
+  }
+
+  public cleanSelect() {
+    this.canvas.cleanSelect();
+  }
+
+  public setCanvasFill() {
+    this.canvas.setCanvasFill();
+  }
+
+  public setCanvasImage() {
+    this.canvas.setCanvasImage();
+  }
+
+  public setId() {
+    this.canvas.setId();
+  }
+
+  public setOpacity() {
+    this.canvas.setOpacity();
+  }
+
+  public setFill() {
+    this.canvas.setFill();
+  }
+
+  public setFontFamily() {
+    this.canvas.setFontFamily();
+  }
+
+  public setTextAlign(value) {
+    this.canvas.setTextAlign(value);
+  }
+
+  public setBold() {
+    this.canvas.setBold();
+  }
+
+  public setFontStyle() {
+    this.canvas.setFontStyle();
+  }
+
+  public hasTextDecoration(value) {
+    this.canvas.hasTextDecoration(value);
+  }
+
+  public setTextDecoration(value) {
+    this.canvas.setTextDecoration(value);
+  }
+
+  public setFontSize() {
+    this.canvas.setFontSize();
+  }
+
+  public setLineHeight() {
+    this.canvas.setLineHeight();
+  }
+
+  public setCharSpacing() {
+    this.canvas.setCharSpacing();
+  }
+
+  public rasterizeJSON() {
+    this.canvas.rasterizeJSON();
   }
 }
